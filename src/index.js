90338840c02afa991e00f25bd2afc592f0711ad9
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-03 22:03:28 +0530

    creating POST req for task and users

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`server running at port ${port}`);
+});
