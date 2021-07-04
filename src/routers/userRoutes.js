e3626647d7ec7932b9e93fe84988d0e412ffd5c9
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-04 20:36:26 +0530

    created seperate routes file for tasks

---
+++ src/routers/userRoutes.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user");
+
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updateKeys = Object.keys(req.body);
+  const updatesAllowed = ["name", "email", "password", "age"];
+
+  const areUpdatesValid = updateKeys.every((update) =>
+    updatesAllowed.includes(update)
+  );
+
+  if (!areUpdatesValid) {
+    return res.status(400).send({ error: "Invalid Update" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
