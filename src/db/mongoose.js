7d19ab3dea7b298804e9dc0e2f95b7f259bbd0a7
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-04 19:36:15 +0530

    removed deprecation warning by setting useFindAndModify: false

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,4 +3,5 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/taskmate", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
