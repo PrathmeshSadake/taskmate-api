87d9ec0ccdf171fb67244f0f399523d774127664
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-04 20:36:34 +0530

    using express router

--- src/index.js
+++ src/index.js
@@ -1,151 +1,15 @@
 const express = require("express");
+
+const userRouter = require("./routers/userRoutes");
+const taskRouter = require("./routers/taskRoutes");
+
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.patch("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  const updateKeys = Object.keys(req.body);
-  const updatesAllowed = ["name", "email", "password", "age"];
-
-  const areUpdatesValid = updateKeys.every((update) =>
-    updatesAllowed.includes(update)
-  );
-
-  if (!areUpdatesValid) {
-    return res.status(400).send({ error: "Invalid Update" });
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.delete("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send("Task not found");
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-app.patch("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  const updateKeys = Object.keys(req.body);
-  const updatesAllowed = ["description", "completed"];
-
-  const areUpdatesValid = updateKeys.every((update) =>
-    updatesAllowed.includes(update)
-  );
-
-  if (!areUpdatesValid) {
-    return res.status(400).send({ error: "Invalid update" });
-  }
-  try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res.status(400).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-app.delete("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findByIdAndDelete(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`server running at port ${port}`);
