852735af2a1f53593a048814dedbca5f983bdabc
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-04 19:36:39 +0530

    Able to do CRUD operations

--- src/index.js
+++ src/index.js
@@ -39,6 +39,46 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updateKeys = Object.keys(req.body);
+  const updatesAllowed = ["name", "email", "password", "age"];
+
+  const areUpdatesValid = updateKeys.every((update) =>
+    updatesAllowed.includes(update)
+  );
+
+  if (!areUpdatesValid) {
+    return res.status(400).send({ error: "Invalid Update" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
@@ -63,6 +103,43 @@ app.get("/tasks/:id", async (req, res) => {
     const task = await Task.findById(_id);
     if (!task) {
       return res.status(404).send("Task not found");
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+app.patch("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updateKeys = Object.keys(req.body);
+  const updatesAllowed = ["description", "completed"];
+
+  const areUpdatesValid = updateKeys.every((update) =>
+    updatesAllowed.includes(update)
+  );
+
+  if (!areUpdatesValid) {
+    return res.status(400).send({ error: "Invalid update" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(400).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+app.delete("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+    if (!task) {
+      return res.status(404).send();
     }
     res.send(task);
   } catch (error) {
