83975454b93ef2d1b28b0f3b356c1173a4cfc1db
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-07-10 13:00:47 +0530

    middlewares

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const taskRouter = require("./routers/taskRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middlewares
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disable");
+//   } else {
+//     next();
+//   }
+// });
+// app.use((req, res, next) => {
+//   res.status(503).send("we're currently under maintainance");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
